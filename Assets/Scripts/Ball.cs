e8abec6f854b2ccd98e1a94cd200c48b6d237c50
jendoebelin <jennifer.doebelin@gmail.com>
2023-06-23 21:40:24 -0500

    next commit

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public bool isPlayer1;
+    public float speed;
+    public Vector3 startPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPosition = transform.position;
+        Launch();
+    }
+    //public void reset function
+    public void Reset()
+    {
+        rb.velocity = Vector2.zero;
+        transform.position = startPosition;
+        Launch();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void Launch()
+    {
+        float x = Random.Range(0, 2) == 0 ? -1 : 1;
+        float y = Random.Range(0, 2) == 0 ? -1 : 1;
+        rb.velocity = new Vector2(speed * x, speed * y);
+    }
+}
+
